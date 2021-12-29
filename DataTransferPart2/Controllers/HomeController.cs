05d183a1a39556bc8e30803f1a2b99cb9f6f7139
TabrizHabiyev <91114214+TabrizHabiyev@users.noreply.github.com>
2021-12-30 02:14:52 +0400

    Add files via upload

---
+++ DataTransferPart2/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using DataTransferPart2.Models;
+using DataTransferPart2.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DataTransferPart2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            #region ViewBag,ViewData,TempData
+            //ViewBag.Name = "Lorem";
+            //List<Student> stu = new List<Student>
+            //{
+            //    new Student{Id=1,Name="Gunel",Age=19},
+            //     new Student{Id=2,Name="Tural",Age=20},
+            //      new Student{Id=3,Name="Habib",Age=25},
+
+            //};
+            //ViewBag.Students = stu;
+            //ViewData["students"] = stu;
+            //ViewBag.Name = "Ibrahim";
+            //ViewData["LastName"] = "Aliyev";
+            //TempData["Age"] = 27;
+            //return RedirectToAction("About");
+            //return RedirectToAction(nameof(About));
+            #endregion
+
+            
+
+            List<Student> stu = new List<Student>
+            {
+                new Student{Id=1,Name="Gunel",Age=19},
+                 new Student{Id=2,Name="Tural",Age=20},
+                  new Student{Id=3,Name="Habib",Age=25},
+
+            };
+            int[] arr = new int[4] { 1,2,4,5};
+            HomeVM homeVM = new HomeVM() { };
+            homeVM.Students = stu;
+            homeVM.arr = arr;
+            return View(homeVM);
+        }
+        public IActionResult About()
+        {
+            TempData["Age"] = 25;
+            return View();
+        }
+    }
+
+
+}
---
+++ DataTransferPart2/ViewModels/HomeVM.cs
@@ -0,0 +1,15 @@
+﻿using DataTransferPart2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DataTransferPart2.ViewModels
+{
+    public class HomeVM
+    {
+        public List<Student> Students { get; set; }
+        public int [] arr { get; set; }
+
+    }
+}
